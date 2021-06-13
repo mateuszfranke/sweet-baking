1b2cf8d6ee250d6f876ec7b7a3d10670f02518aa
mateuszfranke <mateuszfranke@icloud.com>
2021-06-13 14:06:36 +0200

    added tailwind, work on about_me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { FooterComponent } from './footer/footer.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactComponent,
+    AboutMeComponent,
+    FooterComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule
